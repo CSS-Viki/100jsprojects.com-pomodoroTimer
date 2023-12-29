66b0c3c14b08342e00080cfb6c1f31490627bfd9
Viki Designs <vikdemo2023@gmail.com>
2023-12-29 23:50:48 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+Welcome 👋🏽🫂
+
+![Design preview for the pomodoro timer project](./images/pomodoro-timer-project.png)
+
+## Intro
+This is one of the many projetcs from [100jsprojects](https://www.100jsprojects.com/project/pomodoro-timer)
+
+## Project description
+
+The objective of this project is to design a Pomodoro timer that features a countdown clock and three functional buttons, namely start, stop, and reset. Upon clicking the start button, the timer will commence counting down from 25 minutes. Should one desire to halt the countdown, the stop button can be pressed. The countdown can be resumed by hitting the start button, and by clicking on the reset button, the timer will reset to 25 mins again. An alert will notify the user when the timer reaches zero. This Pomodoro timer is an ideal tool for individuals seeking to focus on a task for a set amount of time, followed by a break. By implementing this technique, productivity and time management skills can be improved. 
+
+- Live Site URL: - [Live site](https://fem-pricing-component.onrender.com)
+
+Coded with ❤️ by [Viki Wainaina](https://twitter.com/vykiddeh_)
+
+**Have fun building!** 🚀
---
+++ script.js
@@ -0,0 +1,53 @@
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+let seconds = 60;
+let minutes = 24;
+
+// Variables for setInterval and timer status
+let timerInterval = null;
+let timerStatus = "stopped";
+
+function countDown() {
+  if (seconds === 0) {
+    minutes--;
+    seconds = 60;
+  } else {
+    seconds--;
+    if (minutes === 0 && seconds === 0) {
+      alert("Time ellapsed! Reset timer to start again");
+      stopTimer();
+    }
+  }
+
+  let m = minutes < 10 ? "0" + minutes.toString() : minutes;
+  let s = seconds < 10 ? "0" + seconds.toString() : seconds;
+
+  timer.innerHTML = m + ":" + s;//Display the timer to the DOM
+}
+
+function startTimer() {
+  if (timerStatus === "stopped") {
+    timerInterval = setInterval(countDown, 1);
+    timerStatus = "started";
+  }
+}
+
+function stopTimer() {
+  if (timerStatus === "started") {
+    clearInterval(timerInterval);// To stop the timer
+    timerStatus = "stopped";// To change the status
+  }
+}
+function resetTimer() {
+  clearInterval(timerInterval);
+  let sec = parseInt("00");
+  [minutes, seconds] = [25, sec];
+  timer.innerHTML = `25:00`;
+}
+
+start.addEventListener("click", startTimer);
+reset.addEventListener("click", resetTimer);
+stop.addEventListener("click", stopTimer);
